404dd9b10a8ac2e3d4ace8071102cdeb3e7ade88
spairo <spairo.psychodelic@gmail.com>
2018-05-01 00:56:23 -0500

    feat(home): modal active

--- src/app/home/favorites/favorites.component.ts
+++ src/app/home/favorites/favorites.component.ts
@@ -15,6 +15,8 @@ export class FavoritesComponent implements OnInit {
   @Input() myfavs = [];
   public selected_artist: any;
   public artworks: any;
+  public _overview: any;
+  public image_large: string;
 
   constructor() { }
 
@@ -23,6 +25,14 @@ export class FavoritesComponent implements OnInit {
   selected(id) {
     this.selected_artist = _.find(this.myfavs, {id});
     this.artworks = this.selected_artist.artworks;
+  }
+
+  overview(id) {
+    this._overview = _.find(this.artworks, {id});
+    this._overview._links.image.href = _.replace(
+      this._overview._links.image.href, new RegExp('{image_version}'), this._overview.image_versions[0]
+    );
+    console.log(this._overview._links.image.href);
   }
 
 
